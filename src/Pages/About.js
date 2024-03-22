168294701e0dbef8ddad90c28670136031951a09
Steven Lam <stevenl.inbox@gmail.com>
2024-03-21 19:31:37 -0700

    Modified About page to work with mobile view

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -9,15 +9,6 @@ function About() {
         <div key={index}>
           <h2 className='aboutTitle'>{section.title}</h2>
           <p className='aboutDescription'>{section.description}</p>
-          {section.collaboration && (
-            <p className='aboutDescription'><b>Collaboration: </b>{section.collaboration}</p>
-          )}
-          {section.innovation && (
-            <p className='aboutDescription'><b>Innovation: </b>{section.innovation}</p>
-          )}
-          {section.inclusivity && (
-            <p className='aboutDescription'><b>Inclusivivity: </b>{section.inclusivity}</p>
-          )}
         </div>
       ))}
     </div>
